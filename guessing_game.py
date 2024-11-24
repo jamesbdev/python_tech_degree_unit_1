72bf6fb9610cc047c8cbf033239a3ca1b7ca3327
James Bremner <j.bremner@ymail.com>
2024-11-24 10:35:21 +0000

    add play again functionality

--- guessing_game.py
+++ guessing_game.py
@@ -7,49 +7,58 @@ Project 1 - The Number Guessing Game
 import random
 
 #main function to start the game
-# creates a random number
-# takes input from the user to guess the random number
-# tells the user how many attempts they have made 
+# contains play_game function with the game logic
 def start_game():
     # get the user name
     name = input("What is your name?  ")
     # show a welcome message
     print("***Welcome to the game, {} ***".format(name))
-    # create a random number between 1 and 10
-    random_number = random.randint(1, 10)
-    # set a default guess number before starting
-    guess = 0
-    # set number of attempts
-    attempts = 0
-    replay = False
-    # loop until user finds the correct number
-    while guess != random_number or replay == True:
-        try:
-            guess = int(input("Please choose a number between 1 and 10  "))
-            # increment the number of attempts
-            attempts += 1
-            if guess > 10 or guess < 1:
-                print("The number is between 1 and 10. Please choose again")
-                continue
-            # conditional to check if the guess is greater than the number
-            elif guess > random_number:
-                print("It's lower")
-            # otherwise the guess is lower - show a message to say it's higher
-            elif guess < random_number:
-                print("It's higher")
-        except ValueError: 
-            print("Sorry, it seems you have not entered a number. Please enter a number between 1 and 10")
-    # Show a winning message. Let user know how many attempts they have made.
-    print("Got it. You have made {} attempts".format(attempts))
-    # Show goodbye message
-    print("*** Game Over ***")
-    play_again = input("Would you like to play again? (yes/no)  ")
-    if play_again.lower() == "yes":
-        replay = True
-    else:
-        replay = False
 
-  
+    # contains the logic to loop until the user finds the correct answer
+    def play_game():
+        # create a random number between 1 and 10
+        random_number = random.randint(1, 10)
+        # set a default guess number before starting
+        guess = 0
+        # set number of attempts
+        attempts = 0
+        # loop until user finds the correct number
+        while guess != random_number:
+            try:
+                guess = int(input("Please choose a number between 1 and 10  "))
+                # increment the number of attempts
+                attempts += 1
+                # check if the number is out of range
+                if guess > 10 or guess < 1:
+                    print("The number you chose is out of range. Please choose a number between 1 and 10.")
+                    break
+                # conditional to check if the guess is greater than the number
+                elif guess > random_number:
+                    print("It's lower")
+                # otherwise the guess is lower - show a message to say it's higher
+                elif guess < random_number:
+                    print("It's higher")
+            #handle exception
+            except ValueError: 
+                print("Sorry, it seems you have not entered a number. Please enter a number between 1 and 10")
+        # Show a winning message. Let user know how many attempts they have made.
+        print("Got it. You have made {} attempts".format(attempts))
+        # Show goodbye message
+        print("*** Game Over ***")
+    
+        # function to ask the user if they want to play again, then repeats the game       
+        def play_again():
+            # ask user if they wish to play again
+            play_again = input("Would you like to play again? (yes/no)  ")
+            if play_again.lower() == "yes":
+                #repeat the game logic
+                play_game()
+                
+            else:
+                # Show goodbye message
+                print("Thank you for playing. Bye for now.")
+        play_again()
+    play_game()
 
 
 #Call the function to start the game
